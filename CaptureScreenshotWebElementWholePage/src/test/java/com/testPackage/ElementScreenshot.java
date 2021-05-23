f288dd3ef72a1f0579cf364f360c3a7332b65256
“luismedinacoca” <luismedinacoca@gmail.com>
2021-05-23 13:43:05 -0300

    AUT-004-02-screenshotFullPageAndWebElement added

--- CaptureScreenshotWebElementWholePage/src/test/java/com/testPackage/ElementScreenshot.java
+++ CaptureScreenshotWebElementWholePage/src/test/java/com/testPackage/ElementScreenshot.java
@@ -1,13 +1,13 @@
 package com.testPackage;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 import ru.yandex.qatools.ashot.AShot;
 import ru.yandex.qatools.ashot.Screenshot;
+import ru.yandex.qatools.ashot.coordinates.WebDriverCoordsProvider;
 import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 
 import javax.imageio.ImageIO;
@@ -15,30 +15,30 @@ import java.io.File;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-public class FullScreenShot {
+public class ElementScreenshot {
     public static WebDriver driver;
     String projectPath = System.getProperty("user.dir");
 
     @Test
-    public void fullPageScreenShot() throws InterruptedException {
+    public void elementScreenShot() throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", projectPath + "\\src\\test\\resources\\drivers\\chromedriver.exe");
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.get("https://opensource-demo.orangehrmlive.com");
+        WebElement element = driver.findElement(By.id("btnLogin"));
 
         //create an object of AShot() class and set image strategy by shootingStrategy
         //and viewportPasting() method and take a screenshot using takeScreenshot method
         Screenshot Screenshot = new AShot()
-                .shootingStrategy(ShootingStrategies
-                        .viewportPasting(500)).takeScreenshot(driver);
-
+                .coordsProvider(new WebDriverCoordsProvider())
+                .takeScreenshot(driver, element);
 
         String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
 
         //copy screenshot to desired location
         try{
             ImageIO.write(Screenshot.getImage(), "png",
-                    new File("C:\\Workspace\\AutomationTutorials\\CaptureScreenshotWebElementWholePage\\screenshots\\FullScreenShot_" + dateName + ".png"));
+                    new File("C:\\Workspace\\AutomationTutorials\\CaptureScreenshotWebElementWholePage\\screenshots\\ElementScreenShot_" + dateName + ".png"));
         }catch(Exception e){ e.getMessage();}
 
         driver.close();
